ae9e41373de1f243f17fcada26a1d789f951d8de
Ajay Dange <dangeajay776@gmail.com>
2026-07-27 20:59:11 +0530

    add angular componants

---
+++ src/app/Components/banner-card/banner-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './banner-card.component.html',
+  styleUrl: './banner-card.component.css'
+})
+export class BannerCardComponent {}
---
+++ src/app/Components/header-card/header-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-card.component.html',
+  styleUrl: './header-card.component.css'
+})
+export class HeaderCardComponent {}
