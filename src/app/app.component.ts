71387480bc011c0e6bf333115a10a2f256b2f797
Ajay Dange <dangeajay776@gmail.com>
2026-07-27 21:05:18 +0530

    WIP: Angular UI updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './Components/button/button.component';
+import { CardComponent } from './Components/card/card.component';
+import { Card1Component } from './Components/card1/card1.component';
+import { HeaderCardComponent } from './Components/header-card/header-card.component';
+import { InfoCardComponent } from './Components/info-card/info-card.component';
+import { ProfileCardComponent } from './Components/profile-card/profile-card.component';
+import { FeatureCardComponent } from './Components/feature-card/feature-card.component';
+import { AlertCardComponent } from './Components/alert-card/alert-card.component';
+import { StatsCardComponent } from './Components/stats-card/stats-card.component';
+import { TestimonialCardComponent } from './Components/testimonial-card/testimonial-card.component';
+import { BannerCardComponent } from './Components/banner-card/banner-card.component';
+import { FooterCardComponent } from './Components/footer-card/footer-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    ButtonComponent,
+    CardComponent,
+    Card1Component,
+    HeaderCardComponent,
+    InfoCardComponent,
+    ProfileCardComponent,
+    FeatureCardComponent,
+    AlertCardComponent,
+    StatsCardComponent,
+    TestimonialCardComponent,
+    BannerCardComponent,
+    FooterCardComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'angular-demo';
+
+  constructor() {
+    console.log("Hello Pari");
+  }
 }
